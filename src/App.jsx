5d2adc63ad4661a037e5c9117a86ee5c4b4478ae
Shamblonaut <shaheembn@gmail.com>
2025-05-01 15:09:27 +0530

    feat: setup react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Civilian</h1>
+      <h3>Create enchanting CVs</h3>
+    </>
+  );
+}
+
+export default App;
